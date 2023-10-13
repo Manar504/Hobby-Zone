f25a53a13b476859d78584b5b275df9dbf97e628
Manar_Mamdouh <131297437+Manar504@users.noreply.github.com>
2023-10-14 01:10:00 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main }>
-        <h1>HOBBIES</h1>
+        <h1>Hello form manar</h1>
         
     </main>
   )
