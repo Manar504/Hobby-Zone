8a59328a535790f8b30b861ddf4f60ddc746dbd2
bilalmuhammed99 <belal9971@yahoo.com>
2023-10-14 01:08:20 +0300

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from './Components/Navbar/Navbar'
 
 export default function Home() {
   return (
     <main className={styles.main }>
+       
         <h1>HOBBIES</h1>
-        
     </main>
   )
 }
