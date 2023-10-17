e77e03eed39e77d057bcd964bcf8e2b6ee54962e
Manar_Mamdouh <131297437+Manar504@users.noreply.github.com>
2023-10-18 00:23:33 +0300

    Home

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,22 +1,25 @@
+"use client"
 import React from 'react'
 import "./Navbar.css"
 
 import Link from 'next/link'; 
 import Image from 'next/image';
+import DeafultButton from "../DeafultButton/DeafultButton"
 
-import origami from "../../../Assets/origami.svg"
+import origami from "../../../public/Assets/origami/origami.png"
 
 
 const Navbar = () => {
   return (
     <div>
-
        <nav className="navbar navbar-expand-sm navbar-dark">
      <div className="container">
     <Link className="navbar-brand" href="#"><Image src={origami} alt='hg' width={40} height={40}></Image></Link>
     <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
+   
+    <div className="collapse navbar-collapse" id="collapsibleNavId"> 
     <ul className="navbar-nav  mt-2 mt-lg-0">
         <li className="nav-item">
           <a className="nav-link active" href="#" aria-current="page">   <span className="visually-hidden">(current)</span></a>
@@ -28,8 +31,7 @@ const Navbar = () => {
         <input className="form-control me-sm-2 px-5 rounded-5" type="text" placeholder="Search anything" />
         
       </form> 
-      </ul>
-    <div className="collapse navbar-collapse" id="collapsibleNavId"> 
+      </ul> 
       <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
         <li className="nav-item">
           <Link className="nav-link active" href="/" aria-current="page">Home <span className="visually-hidden">(current)</span></Link>
@@ -39,8 +41,11 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Prices</a>
+        </li>  
+        <li className="nav-item">
+        <DeafultButton  bg="#2945FF" text="Sing Up"  handleClick={(e)=>{e.preventDefault();
+      console.log("manar")}} />
         </li> 
-        
       </ul>
       
       
--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
     <main> 
       <Hero/>
       <Section1/> 
-       
-        
     </main>
   )
 }
