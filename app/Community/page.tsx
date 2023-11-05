e47002b540a12cad3badeca9149b6a5ceb452cbb
Manar_Mamdouh <131297437+Manar504@users.noreply.github.com>
2023-11-05 04:12:23 +0200

    commit

---
+++ app/Community/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from "../Components/post/Post";
+
+// NewsFeed component
+const Community = () => {
+  return (
+    <div className="news-feed">
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+    </div>
+  );
+};
+
+
+
+export default Community;
